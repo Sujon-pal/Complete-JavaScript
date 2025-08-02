219e0dc1bfedd7b8ed7341fe44f2fb9ec6b3f60f
Sujon Pal <dev.sujonpal@gmail.com>
2025-08-02 22:01:43 +0600

    Add files via upload

---
+++ ep-02_Variables/script.js
@@ -0,0 +1,16 @@
+// var example
+var name = "Sujon";
+console.log(name); // Sujon
+name = "Pal";
+console.log(name); // Pal
+
+// let example
+let age = 25;
+console.log(age); // 25
+age = 26;
+console.log(age); // 26
+
+// const example
+const country = "Bangladesh";
+console.log(country); // Bangladesh
+// country = "India"; // ❌ TypeError: can't change const

8edab527b454325ab3e8296881b33c4f5ef0bea0
Sujon Pal <dev.sujonpal@gmail.com>
2025-08-04 22:58:47 +0600

    Add files via upload

---
+++ ep-05_Math_Object/script.js
@@ -0,0 +1,11 @@
+
+
+
+// (Math.random())
+console.log(Math.random());
+
+// user input 
+
+const width = +prompt("Enter your wight")
+const height = +prompt("Enter your height")
+console.log(width * height)
